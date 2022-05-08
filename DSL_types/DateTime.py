ff36fdf8686ff3098250efd99b387fd5c97cb1ad
Yuxuan Li <yxl24@uw.edu>
2022-05-07 22:47:57 -0700

    DSL types

--- DSL_types/DateTime.py
+++ DSL_types/DateTime.py
@@ -1,14 +1,99 @@
-class Date:
-    # possible formats: 'YYYY-MM-DD', 'MM/DD/YYYY', 'YYYY.MM.DD', 'MMM DD, YYYY'...
-    def __init__(self, format='YYYY-MM-DD'):
-        self.fmt = format
+import datetime
 
+import datetime
+class Date:
+    # possible formats: 'YYYY-MM-DD', 'DD/MM/YYYY', 'DD.MM.YYYY', 'MM.DD.YYYY'
+    def __init__(self, format_list=['%Y-%m-%d', '%d.%m.%Y', '%d/%m/%Y', '%m/%d/%Y']):
+        self.fmt_list = format_list
+        now = datetime.datetime.now()
+        self.max_year = now.year
+        self.min_year = now.year
+    
+    def regress(self, data):
+        '''
+        list: [Year, Month, Day, Hour, Minute, Second, Day of Week, Day of Year, Daylight savings time]
+
+        try different format for different data. if all format fail to regre
+        '''
+        for di in data:
+            date = None
+            for fmt in self.fmt_list:
+                try:
+                    date = datetime.datetime.strptime(di, fmt)
+                except ValueError:
+                    pass
+            if (date is None): return False
+            if (date.year > self.max_year): self.max_year = date.year
+            if (date.year < self.min_year): self.min_year = date.year
+        return True
+    
+    def process(self, s):
+        date = None
+        for fmt in  self.fmt_list:
+            try:
+                date = datetime.datetime.strptime(s, fmt)
+                self.fmt = fmt
+            except:
+                pass
+        try:
+            date_list = list(date.timetuple())
+            # check the max_year, min_year
+            if (date_list[0] > self.max_year): date_list[0] = self.max_year
+            if (date_list[0] < self.min_year): date_list[0] = self.min_year
+            return date_list
+        except:
+            print("Error!: incorrect formats, Please check the format of the input you need to process")
 
 class Time:
-    # possible formats: 12 'HH:MM:SS XM', 24 'HH:MM:SS '
-    def __init__(self, format=12):
-        self.fmt = format
+    '''
+    Initialize the Time class
+    Args:
+        int formats: 'HH:MM:SS XM' or 'HH:MM:SS'
+    '''
+    def __init__(self, format_list = ['%H:%M:%S AM', '%H:%M:%S PM', '%H:%M:%S']):
+        now = datetime.datetime.now()
+        self.year = now.year
+        self.month = now.month
+        self.day = now.day
+        self.fmt_list = format_list
+    
+    def regress(self, data):
+        for di in data:
+            date = None
+            for fmt in self.fmt_list:
+                try:
+                    date = datetime.datetime.strptime(di, fmt)
+                except ValueError:
+                    pass
+            if (date is None): return False
+            print(date)
+        return True
+    
+    def process(self, s):
+        '''
+        list: [Year, Month, Day, Hour, Minute, Second, Day of Week, Day of Year, Daylight savings time]
+        process all the time to the same format
+        '''
+        need_add_12 = False
+        if (s[-2] == 'A' or s[-2] == 'P'):
+            if (s[-2] == 'P'): need_add_12 = True
+            s = s[:-3]
+        
+        try:
+            date = datetime.datetime.strptime(s, '%H:%M:%S')
+            date_list = list(date.timetuple())
+            date_list[0] = self.year
+            date_list[1] = self.month
+            date_list[2] = self.day
+            if (need_add_12): date_list[3] += 12
+            return date_list
+        except:
+            print("Error!: incorrect formats, Please check the format of the input you need to process")
 
-class DateTime:
-    def __init__(self):
-        return
+
+class DateTime(Date):
+    def __init__(self, format_list=["%Y-%m-%d %H:%M:%S"]):
+        self.fmt_list = format_list
+        now = datetime.datetime.now()
+        self.max_year = now.year
+        self.min_year = now.year
--- DSL_types/String.py
+++ DSL_types/String.py
@@ -1,34 +1,100 @@
+from asyncio.windows_events import NULL
 import re
 
 class String:
-    def __init__(self, min_len=0, max_len=100, pattern=None):
+    """
+    Initialize the String class
+    
+    Args:
+        int min_len: The minimum length for current String class
+        int max_len: The maximum length for current String class
+        string pattern: The regex pattern of String class
+        
+        "(.*?)" matches anything
+    """
+    def __init__(self, min_len=0, max_len=100, pattern="(.*?)"):
         self.minl = min_len
         self.maxl = max_len
         self.ptrn = pattern
     
     # Notice: must consider pattern match here.
-    # dats is a list.
+    # data is a list.
     # src, src_data_type are checked in map_src.py
     def regress(self, data):
-        # TODO
-        if self.pattern:
-            for di in data:
-                re_match = re.match()
+        '''
+        see if data can regress to current type (class)
+        '''
+        for di in data:
+            if (len(di) < self.minl): self.minl = len(di)
+            if (len(di) > self.maxl): self.maxl = len(di)
+            if self.pattern != "(.*?)":
+                re_match = re.match(self.pattern, di)
                 if not re_match:
                     return False
         return True
 
     def process(self, s):
-        # TODO
+        '''
+        process the string according to attributes
+        '''
         s_ = s
+        assert len(s_) > len(s)
         return s_
 
 class Email(String):
-    def __init__(self, pattern=r'^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b$'):
+    def __init__(self, min_len = 0, max_len = 100, pattern=r'^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b$'):
+        self.minl = min_len
+        self.maxl = max_len
         self.pattern = pattern
         
 
 class Phone_Number(String):
     # for reference: https://softhints.com/regex-phone-number-find-validation-python/
-    def __init__(self, pattern=[r'^[\d]{3}-[\d]{3}-[\d]{3}$', r'^[\d]{3} [\d]{3} [\d]{3}$']):
+    def __init__(self, min_len = 0, max_len = 100, pattern=[r'^[\d]{3}-[\d]{3}-[\d]{4}$', r'^[\d]{3} [\d]{3} [\d]{4}$', r'^\([\d]{3}\) [\d]{3}-[\d]{4}$']):
+        '''
+        Initialize a Phone Number class: e.g. 123 456 7890, 123-456-7890, (123) 456-7890
+        '''
+        self.minl = min_len
+        self.maxl = max_len
+        self.pattern = pattern
+    
+    def regress(self, data):
+        for di in data:
+            if (len(di) < self.minl): self.minl = len(di)
+            if (len(di) > self.maxl): self.maxl = len(di)
+            if self.pattern != "(.*?)":
+                re_match = None
+                # pattern: a list of regex pattern
+                for p in self.pattern:
+                    if (re_match is not None): continue
+                    re_match = re.match(p, di)
+
+                if not re_match:
+                    return False
+        return True
+    
+    def process(self, s):
+        """
+        process all the phone number to the same format
+        """
+        # '(123) 456-7890' => '1234567890'
+        if (re.match(r'^\([\d]{3}\)[\d]{3}-[\d]{4}$', s) is not None):
+            digits_section = re.split(r'[^0-9]',s)[1:]
+        
+        # '123 456 7890' => '1234567890'
+        # '123-456-7890'  => '1234567890'
+        # ‘1234567890’  => '1234567890'
+        else:
+            digits_section = re.split(r'[^0-9]',s)
+        
+        s_ = ""
+        for digits in digits_section:
+            s_ += digits
+        
+        return s_
+
+class URL(String):
+    def __init__(self, min_len = 0, max_len = 100, pattern="((?<=[^a-zA-Z0-9])(?:https?\:\/\/|[a-zA-Z0-9]{1,}\.{1}|\b)(?:\w{1,}\.{1}){1,5}(?:com|org|edu|gov|uk|net|ca|de|jp|fr|au|us|ru|ch|it|nl|se|no|es|mil|iq|io|ac|ly|sm){1}(?:\/[a-zA-Z0-9]{1,})*)"):
+        self.minl = min_len
+        self.maxl = max_len
         self.pattern = pattern
