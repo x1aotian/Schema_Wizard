d43b7945bd5ff5422379a52cbcea33fa70be586c
龚程扬 <gongchengyang@meituan.com>
2022-05-25 15:09:31 -0700

    Django+H5_v1

--- static/design.js
+++ static/design.js
@@ -6,25 +6,25 @@ $(document).ready(function(){
 });
 var context = {
     'type_list': ["Numbers", "Currency", "String", "Email", "Phone_Number", "URL", "Date", "Time", "DateTime"],
-    'type_list_demands': [["prec", "minv", "maxv"], ["curr_type", "prec", "minv", "maxv"], ["minl","maxl"], ["minl","maxl"], ["minl","maxl"], ["minl", "maxl"], ["max_year", "min_year"], [], ["max_year", "min_year"]];
+    'type_list_demands': [["prec", "minv", "maxv"], ["curr_type", "prec", "minv", "maxv"], ["minl","maxl"], ["minl","maxl"], ["minl","maxl"], ["minl", "maxl"], ["max_year", "min_year"], [], ["max_year", "min_year"]]
 }
-/*
-var fieldType = {"Number": ["prec", "minv", "maxv"],
-                "Currency": ["curr_type", "prec", "minv", "maxv"],
-                "String": ["minl","maxl"],
-                "Email": ["minl","maxl"],
-                "Phone_Number": ["minl","maxl"],
-                "URL": ["minl","maxl"],
-                "Date": ["max_year", "min_year"],
-                "Time": [],
-                "DateTime": ["max_year", "min_year"]
-                };
-*/
+
+// var fieldType = {"Number": ["prec", "minv", "maxv"],
+//                 "Currency": ["curr_type", "prec", "minv", "maxv"],
+//                 "String": ["minl","maxl"],
+//                 "Email": ["minl","maxl"],
+//                 "Phone_Number": ["minl","maxl"],
+//                 "URL": ["minl","maxl"],
+//                 "Date": ["max_year", "min_year"],
+//                 "Time": [],
+//                 "DateTime": ["max_year", "min_year"]
+//                 };
+
 
 var keyOptions = "<option value='default'>Select one below</option>";
             
-for (let type in context['type_list']) {
-    keyOptions += "<option value='" + type + "'>" + type + "</option>"
+for (var i = 0; i < context['type_list'].length; i++) {
+    keyOptions += "<option value='" + context['type_list'][i] + "'>" + context['type_list'][i] + "</option>"
 }
 console.log(keyOptions)
 document.getElementById('chooseType').innerHTML = keyOptions;
--- web_schema_wizard/views.py
+++ web_schema_wizard/views.py
@@ -19,32 +19,27 @@ DSL_0 = None
 src_data = None
 src_format = ""
 dest_format = ""
+src_type = None
 # csv_dest_file = ""
 # sql_dest_file = ""
 # sql_table_name_src = ""
 # sql_table_name_dest = ""
 type_option = None
+csv_file = ""
+sql_file = ""
+csv_dest_file = ""
+sql_dest_file = ""
+sql_table_name_dest = ""
 
 def form(request):
     return render(request, 'index.html')
 
 def main(request):
-    context = {}
+    # context = {}
+    # context['res'] = "you succeed!"
+    # return render(request, 'result.html', context)
 
-    if request.method == 'POST':
-        # source = request.POST.get('source')
-        # target = request.POST.get('target')
-        # api_key = request.POST.get('api_key')
-        # base_id = request.POST.get('base_id')
-        # table_name = request.POST.get('table_name')
-        # file_path = request.POST.get('file_path')
-        # converter(source, target, file_path, api_key, base_id, table_name)
-
-        # context['res'] = 'Convert successfully from %s to %s!' % (source, target)
-        context['res'] = 'test successfully'
-        return render(request, 'result.html', context)  # result.html stored locally
-    
-    return render(request, 'index.html', context)
+    return render(request, 'design.html')
 
 def view2(request, id):
     return HttpResponse("You are navigating the view of %s" %id)
@@ -60,11 +55,16 @@ def choose_type(request):
         dest_format = str(request.POST.get('dest'))
         # file_path = str(request.POST.get('myFile'))
         
-        if src_format == "csv":
+        global src_data
+        global src_type
+        global csv_file
+        global sql_file  # just for debugging
+        if src_format == "CSV":
             # print("\n> Please input source file's path. ex: \"samples/csv_test.csv\".")
-            csv_file = str(request.POST.get('csv_src_file')).strip()  
+            csv_file = str(request.POST.get('csv_src_file')).strip() 
+            # csv_file = request.POST.get('csv_src_file') 
             src_data = src_read.read_csv(csv_file)
-        elif src_format == "sql":
+        elif src_format == "SQL":
             # print("\n> Please input source .db's path & table's name, seperated by comma. ex: \"samples/sql_test.db, People\".")
             # sql_file, sql_table = [i.strip() for i in str(input()).split(",")]
             sql_file = str(request.POST.get('sql_src_file')).strip()
@@ -73,14 +73,14 @@ def choose_type(request):
 
         # do regress
         global type_options_proto
-        if src_format == "csv":
+        if src_format == "CSV":
             for col in src_data.columns:
                 type_options_proto.append([])
                 for type in map_src["csv"]["string"]:
                     type_m = type()
                     if type_m.regress(src_data[col]):
                         type_options_proto[-1].append(type_m)
-        elif src_format == "sql":
+        elif src_format == "SQL":
             for i in range(len(src_type)):
                 type_options_proto.append([])
                 nm = src_type.loc[i, 'name']
@@ -94,6 +94,21 @@ def choose_type(request):
         global DSL_0
         DSL_0 = DSL(0)
         DSL_0.names = list(src_data.columns)
+
+        # dest_path should be designated here, because 'request' would change when using other html
+        if dest_format == "CSV":
+            # print("\n> Please input dest file's path. ex: \"samples/csv_test_dest.csv\".")
+            global csv_dest_file
+            csv_dest_file = str(request.POST.get('csv_dest_file'))
+            # dest_write.write_csv(csv_dest_file, DSL_0)
+        elif dest_format == "SQL":
+            # print("\n> Please input source .db's path & table's name, seperated by comma. ex: \"samples/sql_test_dest.db, Students\".")
+            # sql_file, sql_table = [i.strip() for i in str(input()).split(",")]
+            global sql_dest_file
+            global sql_table_name_dest
+            sql_dest_file = str(request.POST.get('sql_dest_file'))
+            sql_table_name_dest = str(request.POST.get('sql_table_name_dest'))
+            # dest_write.write_sql(sql_dest_file, sql_table_name_dest, DSL_0
 
         # context = {'type_options_proto': type_options_proto}
         # return render(request, 'choose_type.html', context)
@@ -159,15 +174,15 @@ def choose_type_mod_loop(request):
         for idx, row in src_data.iterrows():
             DSL_0.addRecord(row)
 
-        if dest_format == "csv":
+        if dest_format == "CSV":
             # print("\n> Please input dest file's path. ex: \"samples/csv_test_dest.csv\".")
-            csv_dest_file = str(request.POST.get('csv_dest_file'))
+            # csv_dest_file = str(request.POST.get('csv_dest_file'))
             dest_write.write_csv(csv_dest_file, DSL_0)
-        elif dest_format == "sql":
+        elif dest_format == "SQL":
             # print("\n> Please input source .db's path & table's name, seperated by comma. ex: \"samples/sql_test_dest.db, Students\".")
             # sql_file, sql_table = [i.strip() for i in str(input()).split(",")]
-            sql_dest_file = str(request.POST.get('sql_dest_file'))
-            sql_table_name_dest = str(request.POST.get('sql_table_name_dest'))
+            # sql_dest_file = str(request.POST.get('sql_dest_file'))
+            # sql_table_name_dest = str(request.POST.get('sql_table_name_dest'))
             dest_write.write_sql(sql_dest_file, sql_table_name_dest, DSL_0)
         context = {}
         context['res'] = 'Convert successfully from %s to %s!' % (src_format, dest_format)
