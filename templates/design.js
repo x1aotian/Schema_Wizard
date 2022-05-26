d8c621568909fbe58e9384ebf6491380f4c2396f
Yuxuan Li <yxl24@uw.edu>
2022-05-25 22:52:44 -0700

    change_type

--- templates/design.js
+++ templates/design.js
@@ -4,26 +4,11 @@ $(document).ready(function(){
         $("#content-"+$(this).val()).removeClass("hidden");
     });
 });
-var context = {
-    'type_list': ["Numbers", "Currency", "String", "Email", "Phone_Number", "URL", "Date", "Time", "DateTime"],
-    'type_list_demands': [["prec", "minv", "maxv"], ["curr_type", "prec", "minv", "maxv"], ["minl","maxl"], ["minl","maxl"], ["minl","maxl"], ["minl", "maxl"], ["max_year", "min_year"], [], ["max_year", "min_year"]]
-}
-
-// var fieldType = {"Number": ["prec", "minv", "maxv"],
-//                 "Currency": ["curr_type", "prec", "minv", "maxv"],
-//                 "String": ["minl","maxl"],
-//                 "Email": ["minl","maxl"],
-//                 "Phone_Number": ["minl","maxl"],
-//                 "URL": ["minl","maxl"],
-//                 "Date": ["max_year", "min_year"],
-//                 "Time": [],
-//                 "DateTime": ["max_year", "min_year"]
-//                 };
-
+var type_list = "{{type_list}}";
 
 var keyOptions = "<option value='default'>Select one below</option>";
             
-for (var i = 0; i < context['type_list'].length; i++) {
-    keyOptions += "<option value='" + context['type_list'][i] + "'>" + context['type_list'][i] + "</option>"
+for (var i = 0; i < type_list.length; i++) {
+    keyOptions += "<option value='" + i + "'>" + type_list[i] + "</option>"
 }
 document.getElementById('chooseType').innerHTML = keyOptions;
