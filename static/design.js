bb1da09609c1cae385f05d29854fe5ae79594122
Yuxuan Li <yxl24@uw.edu>
2022-05-29 14:18:27 -0700

    debug

--- static/design.js
+++ static/design.js
@@ -5,11 +5,13 @@ $(document).ready(function(){
     });
 });
 var val = document.getElementById("type_list").value;
-const type_list = val.slice(1, -1).split(",");
+const type_list = val.slice(1, -1).split(", ");
+
 
 var keyOptions = "<option value='default'>Select one below</option>";
             
 for (var i = 0; i < type_list.length; i++) {
-    keyOptions += "<option value='" + i + "'>" + type_list[i].slice(1,-1) + "</option>"
+    console.log(type_list[i].slice(1,-1));
+    keyOptions += "<option id ='" + i + "' value='" + type_list[i].slice(1,-1) + "'>" + type_list[i].slice(1,-1) + "</option>"
 }
 document.getElementById('chooseType').innerHTML = keyOptions;
